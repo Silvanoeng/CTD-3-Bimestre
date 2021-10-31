2b8dd016f4126105b1f098203c2eb694e565f22f
Silvano AP <silvanoeng@hotmail.com>
2021-10-31 11:47:11 -0300

    Atividade React

---
+++ Front-end03/Aula04/my-app/src/App.js
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import './App.css';
+import homem from './componentes/img/homem-feliz-dinheiro.jpg'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={homem }/>
+      </header>
+    </div>
+  );
+}
+
+export default App;

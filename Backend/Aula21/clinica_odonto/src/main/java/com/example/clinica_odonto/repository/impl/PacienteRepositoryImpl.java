5db3acd96be873e30ab7d23d6f7760f5d84fb793
Silvano AP <silvanoeng@hotmail.com>
2021-11-25 16:39:40 -0300

    aula 25/11

--- Backend/Aula21/clinica_odonto/src/main/java/com/example/clinica_odonto/repository/impl/PacienteRepositoryImpl.java
+++ Backend/Aula21/clinica_odonto/src/main/java/com/example/clinica_odonto/repository/impl/PacienteRepositoryImpl.java
@@ -1,6 +1,8 @@
 package com.example.clinica_odonto.repository.impl;
 
+import com.example.clinica_odonto.dto.DentistaDTO;
 import com.example.clinica_odonto.dto.PacienteDTO;
+import com.example.clinica_odonto.model.Dentista;
 import com.example.clinica_odonto.model.Paciente;
 import com.example.clinica_odonto.repository.IRepositoryDTO;
 import java.util.HashMap;
@@ -34,4 +36,49 @@ public class PacienteRepositoryImpl implements IRepositoryDTO<Paciente> {
 
         return null;
     }
+
+    @Override
+    public Map<Integer, Paciente> buscarTodos() {
+        return null;
+    }
+
+    @Override
+    public void deletar(Integer id) {
+        Paciente pacienteDeletar = buscarPorId(id);
+        enderecoRepository.deletar(pacienteDeletar.getEndereco().getId());
+        pacienteDTOMap.remove(id);
+    }
+
+    @Override
+    public Paciente atualizar(Integer id, Paciente pacienteAtualizar) {
+        if (pacienteAtualizar == null)
+            return null;
+
+        Paciente pacienteBancoDeDados = buscarPorId(id);
+        if (pacienteAtualizar.getNome() != null)
+            pacienteBancoDeDados.setNome(pacienteAtualizar.getNome());
+        if (pacienteAtualizar.getSobrenome() != null)
+            pacienteBancoDeDados.setSobrenome(pacienteAtualizar.getSobrenome());
+        if (pacienteAtualizar.getCpf() != null)
+            pacienteBancoDeDados.setCpf(pacienteAtualizar.getCpf());
+        if (pacienteAtualizar.getEmail() != null)
+            pacienteBancoDeDados.setEmail(pacienteAtualizar.getEmail());
+
+    }
 }
+    @Override
+    public Dentista atualizar(Integer id, Dentista dentistaAtualizar) {
+        if (dentistaAtualizar == null)
+            return null;
+
+        Dentista dentistaBancoDeDados = buscarPorId(id);
+        if (dentistaAtualizar.getMatricula() != null)
+            dentistaBancoDeDados.setMatricula(dentistaAtualizar.getMatricula());
+        if (dentistaAtualizar.getNome() != null)
+            dentistaBancoDeDados.setNome(dentistaAtualizar.getNome());
+        if (dentistaAtualizar.getSobrenome() != null)
+            dentistaBancoDeDados.setSobrenome(dentistaAtualizar.getSobrenome());
+        DentistaDTO dentistaNovo = new DentistaDTO(dentistaBancoDeDados);
+        dentistaDTOMap.put(id, dentistaNovo);
+        return dentistaBancoDeDados;
+    }
